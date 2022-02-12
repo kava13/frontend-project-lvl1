9c7572449d03f54d4f41235e330dce2ecf281b5d
kava13 <kava3331@mail.ru>
2022-02-12 11:11:20 +0300

    Шаг 8

---
+++ bin/brain-progression.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import readlineSync from "readline-sync";
+import { sayWelcome, getRandomInt } from "../src/index.js";
+
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 100;
+
+const createProgression = (firstElem, delta, elementsAmount) => {
+  const progressionArr = [];
+
+  for (let i = 0; i < elementsAmount; i++) {
+    progressionArr.push(firstElem + i * delta);
+  }
+
+  return progressionArr;
+};
+
+const runGame = (name) => {
+  console.log("What number is missing in the progression?");
+
+  for (let i = 0; i < 3; i++) {
+    const firstElem = getRandomInt(50);
+    const delta = getRandomInt(7);
+    const ELEMENTS_AMOUNT = 10;
+
+    // const secondNumber = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
+    const progression = createProgression(firstElem, delta, ELEMENTS_AMOUNT);
+
+    const hiddenElemIndex = getRandomInt(ELEMENTS_AMOUNT - 1);
+    const rightAnswer = String(progression[hiddenElemIndex]);
+    progression[hiddenElemIndex] = "..";
+
+    console.log(`Question: ${progression.join(" ")}`);
+    const answer = readlineSync.question("Your answer: ");
+
+    if (answer === rightAnswer) {
+      console.log("Correct!");
+      if (i === 2) console.log(`Congratulations, ${name}!`);
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${name}`);
+      break;
+    }
+  }
+};
+
+const name = sayWelcome();
+runGame(name);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import readlineSync from "readline-sync";
 
+export const getRandomInt = (maxValue) => Math.floor(Math.random() * maxValue + 1);
+
 export const sayWelcome = () => {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
