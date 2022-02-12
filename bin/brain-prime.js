d8b66314ebbbf3c4bef1e284eaabcf09d4038328
kava13 <kava3331@mail.ru>
2022-02-12 12:19:42 +0300

    Шаг 9

---
+++ bin/brain-prime.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import readlineSync from "readline-sync";
+import { sayWelcome } from "../src/index.js";
+
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 30;
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const runGame = (name) => {
+  console.log("Answer \"yes\" if given number is prime. Otherwise answer \"no\"");
+
+  for (let i = 0; i < 3; i++) {
+    const number = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
+    const rightAnswer = isPrime(number) ? "yes" : "no";
+
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question("Your answer: ");
+    if (answer === rightAnswer) {
+      console.log("Correct!");
+      if (i === 2) console.log("Congratulations!");
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      console.log(`Let's try again, ${name}`);
+      break;
+    }
+  }
+};
+
+const name = sayWelcome();
+runGame(name);
